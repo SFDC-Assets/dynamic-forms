7d1bb8d4d5111c6df623be889f6f9f3fd291a434
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:24:30 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 3 -w 60
-sfdx force:source:push
-sfdx force:user:permset:assign -n AppBuilder
-sfdx force:org:open -p /lightning/o/Vehicle__c/list?filterName=Recent
-sfdx automig:load -d data --concise
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p dynamic -e forms.demo
+sf project deploy start
+sf org assign permset -n AppBuilder
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open -p /lightning/o/Vehicle__c/list?filterName=Recent
+sf automig load -d data --concise
